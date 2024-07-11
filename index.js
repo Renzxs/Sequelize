b56990007f27dac2077e48573e335362e029037b
Renzxs <florencebatol85@gmail.com>
2024-07-11 11:54:56 +0800

    Added Email Validation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const sequelize = require("sequelize");
+const validator = require("validator");
 
 // Models
 const Users = require("./models/Users");
@@ -35,9 +36,11 @@ app.listen(process.env.SERVER_PORT, () => {
 // Default server route
 app.get('/', (req, res) => {
     try {
+        // Success server access
         return res.send(200).json({"message": "Successful Request"});
     }
     catch(error) {
+        // Failed server access
         return res.json({"message": "Internal Server Error"});
     }
 });
@@ -68,78 +71,88 @@ app.get("/get-users", async (req, res) => {
             return res.json({ message: "Successfully fetched a user", result: user });
         }
 
+        // Default query (Gets all users)
         const allUsers = await Users.findAll();
         return res.status(200).json({ message: "Successfully retrieved data", result: allUsers });
 
-    } catch (error) {
+    } 
+    catch (error) {
+        // Incase of an error this will be the response.
         console.error("Error in /get-users endpoint:", error);
         return res.status(500).json({ message: "Internal Server Error"});
     }
 });
 
-
 // Create a user
 app.post("/create-user", async (req, res) => {
-    const { apiKey } = req.query;
     const { fullname, email, password } = req.body;
 
     try {
         if(!fullname || !email || !password) {
+            // If fullname, email or password empty, this will be the response
             return res.json({"message": "Please do not empty the fullname, email and password.", "success": false});
         }
 
-        const createUser = await Users.create({
-            fullname,
-            email,
-            password
-        });
+        if(!validator.isEmail(email)) {
+            // Email Validator
+            return res.json({"message": "Invalid Email address", "success": false});
+        }
 
+        // Create user query
+        const createUser = await Users.create({ fullname, email, password });
         return res.status(201).json({"message": "Successfully Created!", "success": true});
-
-    } catch(error) {
-        console.log(error)
+    } 
+    catch(error) {
+        // Internal server error response
         return res.send(500).json({"message": "Internal Server Error", "success": false});
     }
 });
 
 // Delete a user
 app.delete("/delete-user/:id", async (req, res) => {
-    const { apiKey } = req.query;
     const id = req.params.id;
 
     try {
+        // Get user by id
         const user = await Users.findByPk(id);
-        if(!user) {
-            return res.send(404).json({"message": "User not found"});
-        }
+        if(!user) { return res.send(404).json({"message": "User not found"}); }
+
+        // Delete a user
         await user.destroy();
         return res.json({"message": "User successfully deleted"});      
 
     } catch(error) {
+        // Internal server error
         return res.send(500).json({"message": "Internal Server Error"});
     }
 });
 
 // Edit a user
 app.put("/edit-user/:id", async (req, res) => {
-    const { apiKey } = req.query;
     const id = req.params.id;
     const { fullname, email, password } = req.body;
 
     try {
         if(!fullname || !email || !password){
+            // If fullname, email or password is empty, is will be the server response.
             return res.json({"message": "Please do not empty the fullname, email and password", "success": false});
         }
 
-        const user = await Users.findByPk(id);
-        if(!user) {
-            return res.send(404).json({"message": "User not found", "success": false});
+        if(!validator.isEmail(email)) {
+            // Email Validator
+            return res.json({"message": "Invalid Email address", "success": false});
         }
+
+        // Get user by id query
+        const user = await Users.findByPk(id);
+        if(!user) { return res.send(404).json({"message": "User not found", "success": false}); }
         
+        // Update a user
         await user.update({ fullname, email, password });
         return res.json({"message": "User successfully updated", "success": true});     
 
     } catch(error) {
+        // Internal server error server response.
         return res.send(500).json({"message": "Internal Server Error", "success": false});
     }
 });
